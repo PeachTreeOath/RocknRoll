4fe26b73a1702c41262a7a7418d0f860b8cba246
PeachTreeOath <peachtreeoath@gmail.com>
2016-10-30 03:22:31 -0400

    Time controller + test demo

---
+++ Assets/Resources/Scripts/TimeController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeController : Singleton<TimeController>
+{
+
+    private float currSpeed;
+    private float defaultFixedDeltaTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        defaultFixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            Time.timeScale = TimeSpeed.PAUSED;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Time.timeScale = TimeSpeed.VSLOW;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Time.timeScale = TimeSpeed.SLOW;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Time.timeScale = TimeSpeed.NORMAL;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Time.timeScale = TimeSpeed.FAST;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Time.timeScale = TimeSpeed.VFAST;
+        }
+
+        Time.fixedDeltaTime = Mathf.Clamp(defaultFixedDeltaTime * Time.timeScale, defaultFixedDeltaTime * TimeSpeed.VSLOW, defaultFixedDeltaTime);
+    }
+}
---
+++ Assets/Resources/Scripts/TimeSpeed.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TimeSpeed
+{
+    public const float PAUSED = 0;
+    public const float VSLOW = 0.5f;
+    public const float SLOW = 0.6666f;
+    public const float NORMAL = 1;
+    public const float FAST = 1.5f;
+    public const float VFAST = 2;
+}
