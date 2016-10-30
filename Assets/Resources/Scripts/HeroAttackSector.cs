f0b21e0bc6c1e24e783810d4ce2fe7ca53eeb84b
PeachTreeOath <peachtreeoath@gmail.com>
2016-10-30 04:25:26 -0400

    Attack sector impl of sorts

--- Assets/Resources/Scripts/HeroAttackSector.cs
+++ Assets/Resources/Scripts/HeroAttackSector.cs
@@ -1,15 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeroAttackSector : MonoBehaviour {
+public class HeroAttackSector : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float autoAttackCooldown = 2;
+
+    private float lastAttackTime;
+    private bool attackPerformedThisFrame;
+
+    //TODO clean this up
+    private Material yellowMat;
+    private Material greenMat;
+    private SpriteRenderer spriteRenderer;
+    private Collider2D col;
+
+    // Use this for initialization
+    void Start()
+    {
+        // TODO: Colors are just for testing
+        yellowMat = Resources.Load<Material>("Materials/YellowMat");
+        greenMat = Resources.Load<Material>("Materials/GreenMat");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+
+        ReadyAttack(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastAttackTime > autoAttackCooldown)
+        {
+            ReadyAttack(true);
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (attackPerformedThisFrame)
+        {
+            ReadyAttack(false);
+            attackPerformedThisFrame = false;
+        }
+    }
+
+    {
+        attackPerformedThisFrame = true;
+        //TODO: change this to deal with only enemies
+    }
+
+    private void ReadyAttack(bool enabled)
+    {
+        if (enabled)
+        {
+            spriteRenderer.material = greenMat;
+        }
+        else
+        {
+            spriteRenderer.material = yellowMat;
+        }
+        col.enabled = enabled;
+    }
 }
