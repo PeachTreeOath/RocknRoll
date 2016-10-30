f652e314c0a367564df3e6f21feb572818ce977c
PeachTreeOath <peachtreeoath@gmail.com>
2016-10-30 04:01:58 -0400

    Setup for attack sectors

--- Assets/Resources/Scripts/TimeSpeed.cs
+++ Assets/Resources/Scripts/TimeSpeed.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public static class TimeSpeed
 {
     public const float PAUSED = 0;
-    public const float VSLOW = 0.5f;
-    public const float SLOW = 0.6666f;
+    public const float VSLOW = 0.25f;
+    public const float SLOW = 0.5f;
     public const float NORMAL = 1;
-    public const float FAST = 1.5f;
-    public const float VFAST = 2;
+    public const float FAST = 2;
+    public const float VFAST = 4;
 }
