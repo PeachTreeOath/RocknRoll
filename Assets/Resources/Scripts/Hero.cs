bc1dd25744a648653b1aaaf6f59d5cd63974d545
PeachTreeOath <peachtreeoath@gmail.com>
2016-11-19 03:08:48 -0700

    Basic actives

---
+++ Assets/Resources/Scripts/Hero.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        if (InputController.instance.GetRightClick())
+        {
+            UseActive();
+        }
+    }
+
+    private void UseActive()
+    {
+        transform.localScale *= 2;
+        Invoke("StopActive", 2f);
+    }
+
+    private void StopActive()
+    {
+        transform.localScale /= 2;
+    }
+}
---
+++ Assets/Resources/Scripts/InputController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : Singleton<InputController>
+{
+
+    public bool GetLeftClick()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    public bool GetRightClick()
+    {
+        return Input.GetMouseButtonDown(1);
+    }
+
+}
