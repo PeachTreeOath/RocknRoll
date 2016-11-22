1a18998ccec1666218b038cf58a405bccf4e894c
Fangor <rfonger@gmail.com>
2016-11-22 02:52:07 -0700

    Added walls, and fixed scene cleanup

---
+++ Assets/Resources/Scripts/SceneCleanup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneCleanup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+    }
+}
---
+++ Assets/Resources/Scripts/Wall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    /*
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        IAttackable obj = col.GetComponent<IAttackable>();
+        GameObject gameObj = col.gameObject;
+        if (obj != null)
+        {
+            Vector2 direction = gameObj.transform.position - transform.position;
+            gameObject.GetComponent<IAttackable>().ReceiveAttack(direction, impulseStrength, jitterScale);
+        }
+        
+    }*/
+}
