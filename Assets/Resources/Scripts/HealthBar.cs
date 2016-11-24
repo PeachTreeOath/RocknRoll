9a124179ba4af070315911dd1f9ab468231f2b89
Dave Flanagan <dflanagan314@gmail.com>
2016-11-23 20:03:59 -0500

    Prototype health bar

---
+++ Assets/Resources/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HealthBar : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject hbPrefab;
+
+    [SerializeField]
+    private int numSteps;
+
+    [SerializeField]
+    private float hpPerStep;
+
+    void Start() {
+    }
+
+    void Update() {
+
+    }
+
+    public void changeHealth(float amt) {
+
+    }
+
+}
