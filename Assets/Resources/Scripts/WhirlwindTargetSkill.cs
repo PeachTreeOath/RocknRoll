6d5118f8673c1d4905ba358c6825f8c3537fc1c4
PeachTreeOath <peachtreeoath@gmail.com>
2016-11-25 13:42:14 -0500

    WW targeting skill base

---
+++ Assets/Resources/Scripts/WhirlwindTargetSkill.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class WhirlwindTargetSkill : WhirlwindSkill
+{
+    public override void ExecuteActive(ActiveFinished callback)
+    {
+        //transform.parent.localScale = new Vector2(sizeChange, sizeChange);
+        base.ExecuteActive(callback);
+    }
+    
+}
