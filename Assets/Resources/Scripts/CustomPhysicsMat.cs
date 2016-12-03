57931362acd5806c8da36847612e1b910bdad676
Dave Flanagan <dflanagan314@gmail.com>
2016-12-02 20:32:57 -0500

    WIP RIP

--- Assets/Resources/Scripts/CustomPhysicsMat.cs
+++ Assets/Resources/Scripts/CustomPhysicsMat.cs
@@ -24,6 +24,7 @@ public class CustomPhysicsMat : MonoBehaviour {
 
     /// <summary>
     /// Calculate the two force vectors that result when two objects collide.
+    /// duration is the estimated duration of the contact
     /// IncomingForceOut is the force back in the direction of the initial impulse (i.e. the incoming object's new trajectory).
     /// ReceivingForceOut is the force on the other object
     /// </summary>
@@ -32,22 +33,39 @@ public class CustomPhysicsMat : MonoBehaviour {
     /// <param name="forceIn"></param>
     /// <param name="incomingForceOut"></param>
     /// <param name="receivingForceOut"></param>
-    public void collide(CustomPhysicsMat other, Vector2 surfaceNormal, Vector2 forceIn, out Vector2 incomingForceOut, out Vector2 receivingForceOut) {
+    public void collide(CustomPhysicsMat other, Vector2 hitPt, Vector2 surfaceNormal, Vector2 forceIn, out float duration, out Vector2 incomingForceOut, out Vector2 receivingForceOut) {
 
         //According to fig newton, forces are applied equally, oppositely and tastefully.  Thus, the resulting output forces will be an
         //average of the properties of each input material as a function of the input force.  I have no idea if that is actually true 
         //since I just made all that up.
 
         Vector2 tangent = new Vector2(-surfaceNormal.y, surfaceNormal.x);
+        Debug.Log("Impact normal=" + surfaceNormal + ", tangent=" + tangent);
+        Debug.Break();
         float b = (bounce + other.bounce) / 2;
         float fr = (friction + other.friction) / 2;
 
-        Vector2 forceInDir = forceIn.normalized;
-        Vector2 forceAway = Vector2.Dot(forceInDir, surfaceNormal)* forceIn;
+        float forceInAmt = forceIn.magnitude;
+        Vector2 forceInDir = forceIn / forceInAmt;
+        Vector2 forceOutDir = Vector2.Reflect(forceInDir, surfaceNormal);
+        float fDotn = Vector2.Dot(forceInDir, surfaceNormal);
+        Vector2 forceAway =  fDotn * forceInAmt * forceOutDir;
         Vector2 forceAlong = Vector2.Dot(forceInDir, tangent) * forceIn;
 
-        incomingForceOut = forceAway * b - forceAlong * fr;
-        receivingForceOut = new Vector2(incomingForceOut.y, incomingForceOut.x);
+        Vector2 totalForceOut = forceAway * b - forceAlong * fr;
+        //TODO outgoing force should be capped to not allow the object to fall through?
+        Debug.Log("incomingForce=" + forceIn + ", amt=" + forceInAmt);
+        Debug.Log("forceOutDir=" + forceOutDir + ", fullAmt=" + forceAway);
+        Debug.Log("incomingForceOut=" + totalForceOut);
+        float EPS2 = 0.45f; //wiggle room
+        Vector2 zeroForce = -fDotn * (forceInAmt+EPS2) * surfaceNormal; //need to have this output otherwise collision will pass through
+        Debug.Log("zeroForce=" + zeroForce);
+        float xOut = Math.Abs(totalForceOut.x) > Math.Abs(zeroForce.x) ? totalForceOut.x : zeroForce.x;
+        float yOut = Math.Abs(totalForceOut.y) > Math.Abs(zeroForce.y) ? totalForceOut.y : zeroForce.y;
+        incomingForceOut = new Vector2(xOut, yOut);
+        receivingForceOut = Vector2.zero; //TODO
+        //TODO duration based on speed, angle, how soft the object is.
+        duration = 0.75f;
     }
 
 }
--- Assets/Resources/Scripts/Enemy.cs
+++ Assets/Resources/Scripts/Enemy.cs
@@ -11,17 +11,18 @@ public class Enemy : MonoBehaviour, IAttackable
     private float maxExtent;
     private CustomPhysics phys;
 
-    private float myMass = 0.01f;
-    private Vector2 positionLastFrame;
-    private Vector2 velocityLastFrame;
-    private Vector2 accelLastFrame;
+    private float myMass = 1f;
+    private Vector2 positionLastFrame;  //initial position
+    private Vector2 velocityLastFrame;  //initial velocity
+    private Vector2 constAccel;
     private Vector2 netAccelToApplyWc;
     private Vector2 positionAfterUpdate;
+    private Vector2 velocityAfterUpdate;
     private bool positionAfterUpdateCalculated;
+    private float lastUpdateTime;
+    private float minTimeStep = 0.025f;
+    private Vector2 gravityAccel = new Vector2(0, -1.0f);
 
-    private Vector2 gravityAccel = new Vector2(0, -0.1f);
-
-    // Use this for initialization
     void Start()
     {
         physicsLayer = LayerMask.GetMask("Physics");
@@ -30,65 +31,68 @@ public class Enemy : MonoBehaviour, IAttackable
         maxExtent = (collider.bounds.extents.x + collider.bounds.extents.x + 0.01f) / 2; //e.g. radius, approximate
         phys = GetComponent<CustomPhysics>();
         positionLastFrame = transform.position;
+        positionAfterUpdate = transform.position;
         positionAfterUpdateCalculated = false;
         netAccelToApplyWc = Vector2.zero;
         velocityLastFrame = Vector2.zero;
-        accelLastFrame = gravityAccel;
+        velocityAfterUpdate = Vector2.zero;
+        constAccel = gravityAccel;
+        lastUpdateTime = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //apply gravity
-        //netAccelToApplyWc; += new Vector2(0, -0.01f); //This may need to be applied later, maybe in late update?
-        Vector2 nextPos = getNextPosition();
-        checkCollisions(nextPos);
-        adjustNextPosition(netAccelToApplyWc);
+        float dt = Time.time - lastUpdateTime;
+        if (dt >= minTimeStep) {
+            //Debug.Log("timeStep=" + dt);
+
+            //Figure out where we will be next frame (actually at the very end of this frame)
+            //This is a combination of our current position and any force that will be applied by an external object
+
+            Vector2 idealNextPos = getNextPosition(dt, Vector2.zero);
+            checkCollisions(idealNextPos);
+
+            //figure out the real next position including collision data (note this can be skipped if no collisions were detected)
+            getNextPosition(dt, netAccelToApplyWc);
+
+            lastUpdateTime = Time.time;
+        }
 
 
 
     }
 
     void LateUpdate() {
-        velocityLastFrame = (positionAfterUpdate - (Vector2)transform.position) / Time.deltaTime; //TODO time is off by 1 frame
+        //velocityLastFrame = (positionAfterUpdate - (Vector2)transform.position) / Time.deltaTime; //TODO time is off by 1 frame
+        velocityLastFrame = velocityAfterUpdate;
         positionLastFrame = transform.position;
         transform.position = positionAfterUpdate;
         netAccelToApplyWc = Vector2.zero;
+        positionAfterUpdateCalculated = false;
     }
 
     /// <summary>
     /// Determine the position of this object at the end of the current frame based on forces applied
     /// during this frame. Calculations for all object interactions are done with the objects in the state
     /// of the current frame.
+    /// This is equivalent to the position the object would be in if undisturbed by any other objects.
     /// </summary>
-    public Vector2 getNextPosition() {
+    public Vector2 getNextPosition(float dt, Vector2 addForce) {
 
-        //Vector2 mvDirection = (Vector2) transform.position - positionLastFrame;
-        //Vector2 pos = (Vector2) transform.position + netAccelToApplyWc
-        //Vector2 pos = velocityLastFrame * Time.deltaTime + positionLastFrame;  // vt + p
-        Vector2 pos =  accelLastFrame * (0.5f * Mathf.Pow(Time.deltaTime, Time.deltaTime)) +
-                        velocityLastFrame * Time.deltaTime + 
-                        positionLastFrame;  // 1/2 at^2 + vt + p 
+        // pos = 1/2 at^2 + vt + p. Since we are using a time step instead of cumulative time we need
+        //to factor in the previous state then add our deltas within the last time step. 
+        Vector2 vel = velocityLastFrame + (constAccel+addForce) * dt;
+        Vector2 pos =  positionLastFrame + vel * dt; 
 
+        velocityAfterUpdate = vel;
         positionAfterUpdate = pos;
         positionAfterUpdateCalculated = true;
         return positionAfterUpdate;
     }
-
-    public Vector2 adjustNextPosition(Vector2 accelUpdates) {
-        //This is prototype code only. 
-        Vector2 pos =  (accelLastFrame+accelUpdates) * (0.5f * Mathf.Pow(Time.deltaTime, Time.deltaTime)) +
-                        velocityLastFrame * Time.deltaTime + 
-                        positionLastFrame;  // 1/2 at^2 + vt + p 
-
-        positionAfterUpdate = pos;
-        positionAfterUpdateCalculated = true;
-        return positionAfterUpdate;
-    }
-
 
     /// <summary>
-    /// Check for objects between the current position and the targetPos
+    /// Check for objects between the current position and the targetPos. Currently this only looks at the 
+    /// positions for this time step.
     /// </summary>
     private void checkCollisions(Vector2 targetPos) {
         Vector2 dir = targetPos - (Vector2)transform.position;
@@ -104,13 +108,13 @@ public class Enemy : MonoBehaviour, IAttackable
         //cast from the center and one from each side along the current trajectory
         hitInfo[0] = Physics2D.Raycast(exMid, dir, maxDist, physicsLayer.value);
         //int results = collider.Raycast(dir, hitInfo, maxDist, physicsLayer.value);
-        Debug.DrawRay(exMid, dir * maxDist, Color.cyan, 1.0f, false);
+        //Debug.DrawRay(exMid, dir * maxDist, Color.cyan, 1.0f, false);
 
         hitInfo[1] = Physics2D.Raycast(ex1, dir, maxDist, physicsLayer.value);
-        Debug.DrawRay(ex1, dir * maxDist, Color.green, 1.0f, false);
+        //Debug.DrawRay(ex1, dir * maxDist, Color.green, 1.0f, false);
 
         hitInfo[2] = Physics2D.Raycast(ex2, dir, maxDist, physicsLayer.value);
-        Debug.DrawRay(ex2, dir * maxDist, Color.red, 1.0f, false);
+        //Debug.DrawRay(ex2, dir * maxDist, Color.red, 1.0f, false);
 
         //Debug.Log("Num results = " + results);
         GameObject goHit;
@@ -119,9 +123,9 @@ public class Enemy : MonoBehaviour, IAttackable
             if (hitInfo[i]) {
                 if (collider.bounds.Intersects(hitInfo[i].collider.bounds)) {
                     //if (collider.IsTouching(hitInfo[i].collider)) { //only works with physics
-                    Debug.Log("myBounds=" + collider.bounds);
-                    Debug.Log("otherBounds=" + hitInfo[i].collider.bounds);
-                    Debug.Log("Hit some shit: [" + i + "] =>" + hitInfo[i].collider.gameObject.name);
+                    //Debug.Log("myBounds=" + collider.bounds);
+                    //Debug.Log("otherBounds=" + hitInfo[i].collider.bounds);
+                    //Debug.Log("Hit some shit: [" + i + "] =>" + hitInfo[i].collider.gameObject.name);
                     goHit = hitInfo[i].collider.gameObject;
                     CustomPhysics gop = goHit.GetComponent<CustomPhysics>();
                     if (gop != null) {
@@ -130,9 +134,12 @@ public class Enemy : MonoBehaviour, IAttackable
                         Vector2 forceIn = velocityLastFrame * myMass; //TODO this is current V not V in the future when the collision happens
                         Vector2 myForceOut;
                         Vector2 otherForceOut;
-                        gop.curMat.collide(phys.curMat, n, forceIn, out myForceOut, out otherForceOut);
+                        float duration;
+                        gop.curMat.collide(phys.curMat, hitPt, n, forceIn, out duration, out myForceOut, out otherForceOut);
+                        myForceOut *= duration;
                         Debug.Log("MyForceOut=" + myForceOut);
-                        netAccelToApplyWc += myForceOut / myMass; // F = ma
+                        //netAccelToApplyWc += myForceOut / myMass; // F = ma
+                        netAccelToApplyWc += myForceOut ; // F = ma
                         //Debug.Break();
                     }
                 }
@@ -146,7 +153,8 @@ public class Enemy : MonoBehaviour, IAttackable
         Vector2 jitterVector = UnityEngine.Random.insideUnitCircle * 0.1f; //wtf is this
         Vector2 force = jitterVector + direction + (direction.normalized * impulseStrength);
         Vector2 forceOut;
-        phys.curMat.collide(otherPhys.curMat, surfaceNormal, force, out forceOut, out myOut); 
+        float d;
+        phys.curMat.collide(otherPhys.curMat, Vector2.zero, surfaceNormal, force, out d, out forceOut, out myOut); 
 
         //rBody.AddForce(myOut, ForceMode2D.Impulse);
         //rBody.AddForce(direction * impulseStrength, ForceMode2D.Impulse);
